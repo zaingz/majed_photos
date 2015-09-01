97af4b5df2687b002b1db4c7310b9b6c395efa88
zaingz <zaing.143@gmail.com>
2015-09-01 17:26:28 +0500

    Final upload

--- app/assets/javascripts/components/OverlayMenu.js.jsx
+++ app/assets/javascripts/components/OverlayMenu.js.jsx
@@ -1,6 +1,17 @@
 var OverlayMenu
  = React.createClass({
 
+    componentDidMount: function() {
+        $(".icon").click(function () {
+            $(".mobilenav").fadeToggle(500);
+            $(".top-menu").toggleClass("top-animate");
+            $(".mid-menu").toggleClass("mid-animate");
+            $(".bottom-menu").toggleClass("bottom-animate");
+        });
+
+        new WOW().init();
+        //end od ready
+    },
 
 	render: function(){
 
@@ -10,12 +21,15 @@ var OverlayMenu
 					<div className="overlay_menu container">
 
 						<div className="mobilenav"> 
-						  <li><a href="#"  data-hover="Portfolio">Portfolio</a></li>
-						  <li><a href="#"  data-hover="Projects">Projects</a></li>
-						  <li><a href="#"  data-hover="Blog">Blog</a></li>
-						  <li><a href="#"  data-hover="Events">Events</a></li>
-						  <li><a href="#"  data-hover="Contact">Contact</a></li>
-						  <li><a href="#"  data-hover="Event">Event</a></li>
+
+						  <li><a href="/projects"  data-hover="Projects">Projects</a></li>
+						  <li><a href="/blog"  data-hover="Blog">Blog</a></li>
+						  <li><a href="/events"  data-hover="Events">Events</a></li>
+						  <li><a href="/contact"  data-hover="Contact">Contact</a></li>
+						  <li><a href="/videos"  data-hover="Videos">Videos </a></li>
+                          <li><a href="/about"  data-hover="About">About</a></li>
+
+
 						</div>
 
 
---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,6 @@
+class EventController < ApplicationController
+  def index
+  end
+  def show
+  end
+end
